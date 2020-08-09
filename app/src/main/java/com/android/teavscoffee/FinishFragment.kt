0609d0d76efcd9215382204bfbc8a7e7f3a43dc4
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-09 14:16:48 +0700

    Menginstall safeargs

---
+++ app/src/main/java/com/android/teavscoffee/FinishFragment.kt
@@ -0,0 +1,24 @@
+package com.android.teavscoffee
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.android.teavscoffee.databinding.FragmentFinishBinding
+
+class FinishFragment : Fragment() {
+    private lateinit var dataBinding: FragmentFinishBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_finish, container, false)
+        dataBinding.apply {
+            return root
+        }
+    }
+
+}
