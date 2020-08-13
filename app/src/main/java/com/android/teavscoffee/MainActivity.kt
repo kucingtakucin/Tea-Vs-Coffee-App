f46219bb7f5fd38c43398f87881e24daaebc256e
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-08-13 13:14:30 +0700

    Menambahkan bottom navigation

--- app/src/main/java/com/android/teavscoffee/MainActivity.kt
+++ app/src/main/java/com/android/teavscoffee/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.teavscoffee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -10,15 +11,25 @@ import com.android.teavscoffee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var dataBinding: ActivityMainBinding
-    private val navController: NavController = this.findNavController(R.id.nav_host_fragment)
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
-        NavigationUI.setupActionBarWithNavController(this@MainActivity, navController)
+        drawerLayout = dataBinding.drawerLayout
+    }
+
+    override fun onStart() {
+        super.onStart()
+        navController = findNavController(R.id.nav_host_fragment_container)
+        NavigationUI.setupActionBarWithNavController(this@MainActivity, navController, drawerLayout)
+        NavigationUI.setupWithNavController(dataBinding.navigationView, navController)
+        NavigationUI.setupWithNavController(dataBinding.bottomNav, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        navController = findNavController(R.id.nav_host_fragment_container)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
